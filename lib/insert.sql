ddef5f323df942026d1fe1a0c1a3749142d633a5
Tri <trihoang09@gmail.com>
2023-09-18 15:01:01 -0600

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES(1,'Mr. Chocolate',18,'M','Black','Calm',True);
+INSERT INTO bears VALUES(2,'Rowdy',18,'M','Black','Calm',True);
+INSERT INTO bears VALUES(3,'Tabitha',18,'M','Black','Calm',True);
+INSERT INTO bears VALUES(4,'Sergeant Brown',18,'M','Black','Calm',True);
+INSERT INTO bears VALUES(5,'Melissa',18,'M','Black','Calm',True);
+INSERT INTO bears VALUES(6,'Grinch',18,'M','Black','Calm',True);
+INSERT INTO bears VALUES(7,'Wendy',18,'M','Black','Calm',True);
+INSERT INTO bears VALUES(8,NULL,18,'M','Black','Calm',True);
